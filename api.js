8431a04dc5ec4fe05e3d5f1cc71b75a3744dc376
Thalys001 <thalyssouza708@gmail.com>
2023-05-04 22:43:00 -0300

    feat: adding api key

---
+++ api.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'api_key=399681cd4e2ca9464a1c616336559ad8';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
